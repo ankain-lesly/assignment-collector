b6261999fe1d34d5faad5db25740a82f279fb34e
Ankain Lesly <leeleslyank@gmail.com>
2024-02-19 03:15:37 +0100

    new structure

---
+++ public/partials/links.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en" class="dark">
+
+<head>
+  <meta charset="utf-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1" />
+  <meta name="theme-color" content="#000000" />
+
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <meta name="author" content="Devlee @local logistics">
+  <!-- favicon -->
+  <link rel="apple-touch-icon" sizes="180x180" href="/static/favicon/apple-touch-icon.png">
+  <link rel="icon" type="image/png" sizes="32x32" href="/static/favicon/favicon-32x32.png">
+  <link rel="icon" type="image/png" sizes="16x16" href="/static/favicon/favicon-16x16.png">
+  <link rel="manifest" href="/static/favicon/site.webmanifest">
+
+  <!-- <meta name="robot" content="index,follow"> -->
+  <meta name="description" content="Submit your assignment before Mon 13 - 6:30 PM - COURSE TITLE - COURSE CODE">
+
+  <title>Assignment Collector | DevLee</title>
+
+  <link rel="icon" href="/favicon.ico" />
+
+  <!-- // DEPLOY Dev -->
+  <script src="https://kit.fontawesome.com/8ea543231b.js" crossorigin="anonymous"></script>
+
+  <!-- // LOCAL DEV -->
+  <!-- <link rel="stylesheet" href="/_00FA/css/all.css" /> -->
+  <!-- <link rel="stylesheet" href="/ass-static/styles/_fonts.css"> -->
+
+  <link rel="stylesheet" href="/ass-static/styles/main-style.css">
+</head>
